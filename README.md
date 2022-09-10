7338ea5959df7c23f921dc4af8bca6baaac1f9ba
Vladimir Sushkov <volodya.sushkov@gmail.com>
2022-09-10 20:49:43 +0300

    feat: add goerli deployment, update README.md

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Simple contract allowed 1:1 swap of one ERC20 token to another.
 
 ## For Developers
 
+When deploying, use the following environment:
+
+- Node: v16.14.0  (Contract verification (truffle-plugin-verify) works well with Node: v10.15.3)
+- Npm: 8.12.1
+- Truffle: 5.1.56
+
+First you need to set the values in ```.env``` and configure network in ```truffle-config.js```. Then execute at the root:
+
 ### Install dependencies
 
 ```sh
@@ -21,6 +29,22 @@ npm run compile
 
 ```sh
 npm run test:truffle
+```
+
+### Deployment
+
+Before executing migrations, it is necessary to review the migration code and set the required values.
+
+```sh
+npx truffle migrate --f 2 --to 2 --network {network}
+```
+
+### Etherscan verification of contract
+
+Configure ```ETHERSCAN_API_KEY``` variable in ```.env``` file.
+
+```bash
+npx truffle run verify {contract_name}@{contract_address} --network {network}
 ```
 
 ## License
--- migrations/2_deploy_token_swaps.js
+++ migrations/2_deploy_token_swaps.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-const {
-    BN,
-    ether
-} = require("@openzeppelin/test-helpers");
-
 const TokenSwaps = artifacts.require("TokenSwaps.sol");
-const IERC20 = artifacts.require("IERC20.sol");
+
+const tokenFrom = ""; // TODO: set
+const tokenTo = ""; // TODO: set
+const ownerAddress = ""; // TODO: set
+const walletAddress = ""; // TODO: set
 
 // TokenSwaps deployment
 async function swapDeploy(
@@ -44,95 +43,27 @@ module.exports = async function(deployer, network) {
     const contractsAddresses = {};
     const contractsAbi = {};
 
-    if (network === "kovan") {
-        const TOKEN_FROM_ADDRESS = "0x42Bbfc77Ee4Ed0efC608634859a672D0cf49e1b4"; // Kovan tCUR
-        const TOKEN_TO_ADDRESS = "0x2f4d4cFAb714e4573189B300293944673Fe0efF7";   // Kovan tCGT
+    // 1. TokenSwaps deployment
+    let tokenSwaps = await swapDeploy(
+      deployer,
+      tokenFrom,
+      tokenTo,
+      walletAddress
+    );
 
-        const OWNER = "0xB844C65F3E161061bA5D5dD8497B3C04B71c4c83";
-        const WALLET = "0xB844C65F3E161061bA5D5dD8497B3C04B71c4c83";
+    // 2. Set owner for TokenSwaps
+    await setOwner(
+      tokenSwaps,
+      ownerAddress
+    );
 
-        const tokenToAmt = ether(new BN(2e6));  // tCGT for swaps
+    // 3. Save TokenSwaps address
+    contractsAddresses.tokenSwaps = tokenSwaps.address;
+    contractsAddresses.tokenFrom = tokenFrom;
+    contractsAddresses.tokenTo = tokenTo;
 
-
-
-        console.log(">>> DEPLOYING TokenSwaps");
-
-        // 1. TokenSwaps deployment
-        let tokenSwaps = await swapDeploy(
-            deployer,
-            TOKEN_FROM_ADDRESS,
-            TOKEN_TO_ADDRESS,
-            WALLET
-        );
-
-        // 2. Transfer tokenTo to TokenSwaps
-        let tokenTo = await IERC20.at(TOKEN_TO_ADDRESS);
-        await tokenTo.transfer(
-            tokenSwaps.address,
-            tokenToAmt
-        );
-
-        // 3. Set owner
-        await setOwner(
-            tokenSwaps,
-            OWNER
-        );
-
-        // 4. Use swap
-        let tokenFrom = await IERC20.at(TOKEN_FROM_ADDRESS);
-        await tokenFrom.approve(
-            tokenSwaps.address,
-            ether("1")
-        );
-        await tokenSwaps.swap(
-            ether("1")
-        );
-
-        // 5. Save TokenSwaps address
-        contractsAddresses.tokenSwaps = tokenSwaps.address;
-        contractsAddresses.tokenFrom = tokenFrom.address;
-        contractsAddresses.tokenTo = tokenTo.address;
-
-        // 6. Save TokenSwaps ABI
-        contractsAbi.tokenSwaps = tokenSwaps.abi;
-
-        console.log(">>> END DEPLOYING TokenSwaps");
-    } else if (network === "mainnet") {
-        // Main addresses setup
-        const TOKEN_FROM_ADDRESS = "0x13339fD07934CD674269726EdF3B5ccEE9DD93de"; // Mainnet CUR
-        const TOKEN_TO_ADDRESS = "0xF56b164efd3CFc02BA739b719B6526A6FA1cA32a"; // Mainnet CGT
-
-        const OWNER = "0x94ddecAFF0109b615e51C482e07312abce704042"; // Development fund multisig
-        const WALLET = "0x42A9bF91864b1cd0FCcF6D8292c94758C69d440F"; // Destination wallet for CUR
-
-
-
-        console.log(">>> DEPLOYING TokenSwaps");
-
-        // 1. TokenSwaps deployment
-        let tokenSwaps = await swapDeploy(
-            deployer,
-            TOKEN_FROM_ADDRESS,
-            TOKEN_TO_ADDRESS,
-            WALLET
-        );
-
-        // 2. Set owner for TokenSwaps
-        await setOwner(
-            tokenSwaps,
-            OWNER
-        );
-
-        // 3. Save TokenSwaps address
-        contractsAddresses.tokenSwaps = tokenSwaps.address;
-        contractsAddresses.tokenFrom = TOKEN_FROM_ADDRESS;
-        contractsAddresses.tokenTo = TOKEN_TO_ADDRESS;
-
-        // 4. Save TokenSwaps ABI
-        contractsAbi.tokenSwaps = tokenSwaps.abi;
-
-        console.log(">>> END DEPLOYING TokenSwaps");
-    }
+    // 4. Save TokenSwaps ABI
+    contractsAbi.tokenSwaps = tokenSwaps.abi;
 
     // Write addresses and ABI to files
     if (contractsAddresses && contractsAbi) {
